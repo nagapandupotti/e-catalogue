a32a76aae78610d699c9c88ac2c9fac8c55824b1
Nagapandu Potti <nagapandupotti@gmail.com>
2015-12-27 17:41:17 -0500

    Push complete source code to newly created repository

---
+++ src/desktopapplication1/createImageIcon.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package desktopapplication1;
+
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author ACER
+ */
+class createImageIcon extends ImageIcon {
+
+    public createImageIcon(String string, String string0) {
+    }
+    
+}
---
+++ src/desktopapplication1/jLabel1.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package desktopapplication1;
+
+/**
+ *
+ * @author ACER
+ */
+class jLabel1 {
+
+    static class setText {
+
+        public setText() {
+        }
+    }    
+}
